deb2efb41e729c887ce562c9fad2bfcd73366bc2
Arley Lobato <arleylobato@gmail.com>
2025-01-03 14:39:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { NativeEventEmitter, NativeModules } = ReactNative;
 
 const RNZipArchive = NativeModules.RNZipArchive;
 
-// const rnzaEmitter = new NativeEventEmitter(RNZipArchive);
-const rnzaEmitter = new NativeEventEmitter();
+const rnzaEmitter = new NativeEventEmitter(RNZipArchive);
+// const rnzaEmitter = new NativeEventEmitter();
 
 const normalizeFilePath = (path) =>
   path.startsWith("file://") ? path.slice(7) : path;
